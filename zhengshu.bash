49ff08e2cf42677bc4018c2c4eb8dc0aa5c917af
xiaoxuenice <50929483+xiaoxuenice@users.noreply.github.com>
2020-09-02 11:08:18 +0800

    Update zhengshu.bash

--- zhengshu.bash
+++ zhengshu.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i  in `find . -name *nginx*`;do unzip $i;done
-for i in `find . -name "*.crt" |awk -F'.' '{print $3"."$4}' `;do cp /usr/local/nginx/kis/188ball.net.conf ${i}.conf && sed -i "s/188ball\.net/$i/g" ${i}.conf;done
+for i in `find . -name "*.crt" |awk -F'.' '{print $3"."$4}' `;do cp /usr/local/nginx/kis/jd.com.conf ${i}.conf && sed -i "s/jd.com/$i/g" ${i}.conf;done
 #find . -type f |xargs sed -i 's///g;s///g'
