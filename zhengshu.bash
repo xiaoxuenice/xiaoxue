eea2cfb0b01728e0553c122d59c11384d9628387
zhangsfdsaf <xiaoxuenice@qq.com>
2020-12-02 10:59:29 +0800

    delete

--- zhengshu.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i  in `find . -name *nginx*`;do unzip $i;done
-for i in `find . -name "*.crt" |awk -F'.' '{print $3"."$4}' `;do cp /usr/local/nginx/kis/jd.com.conf ${i}.conf && sed -i "s/jd.com/$i/g" ${i}.conf;done
-#find . -type f |xargs sed -i 's///g;s///g'
